dac2741dfa7bee184d9fae4d4957a86010b26a86
Théau <theaumery@gmail.com>
2021-04-28 15:08:51 +0200

    🚧WIP:Tests unitaires

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port : 3000,
+    port : process.env.PORT || 3000,
     appKey: 'cf0d9c3385323d6c70d38357a8aa9385a'
 };
--- test.js
+++ test.js
@@ -1,5 +1,23 @@
 const assert = require('assert');
-it('simple test', (done) => {
-  assert.ok(true);
-  done();
+const repoUser = require('./src/repository/User.js')
+
+
+
+it('should be true if email exist', (done) => {
+    (new repoUser).userGetEmail('foiehjfoei@gmail.com').then((result) => {
+        assert.notStrictEqual(result, null);
+        assert.strictEqual(result.email, 'foiehjfoei@gmail.com');
+        done();
+    });
+
+});
+
+it("should be true if email doesn't exist", (done) => { 
+    (new repoUser).userGetEmail('foiehjfoei@gmail.com').then(() => {
+        //-- on ne tombera pas ici car on test un email inexistant 
+    },(msg) => {
+        assert.ok(true)
+        //assert.strictEqual(msg, `Utilisateur non trouvé`);
+        done();
+    });
 });
